cac5bedd7e52a4a77d9f0e0b3644c61caae33d5d
anu-mukundarao <anu.m@mphasis.com>
2020-09-06 05:35:17 +0530

    breaking down components

---
+++ breaking_down_components/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {CardList} from './components/card-list/card-list.components';
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[]
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(users=>this.setState({monsters:users}))
+  }
+  render(){
+    return (
+      <div className="App">
+        {/* listing the cards */}
+        <CardList monsters={this.state.monsters}/> 
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
