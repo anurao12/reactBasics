6fb893aa9016048750a10febbd45f82757aacfa7
anu-mukundarao <anu.m@mphasis.com>
2020-09-08 13:59:01 +0530

    component based ecommerce app with background image growing style

---
+++ ecommerce-components/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Directory from '../../component/directory/directory.component';
+import '../homepage/homepage.style.scss';
+
+export const HomePage = () =>(
+    <div className='homepage'>
+        <Directory />
+    </div>
+    
+);
